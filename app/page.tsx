b66daf3d05bc25883dcf42231d63de89532b61a0
Ryan Furrer <ryandotfurrer@gmail.com>
2025-08-12 22:22:56 -0400

    - add placehodler logo in navbar - add ability to click on task name and mark task as complete - various styling changes - improve no tasks state look - improve task list with button wrapper and the ability to click on the task name to mark the task as complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,72 @@
 "use client";
 
+import { Greeting } from "@/components/Greeting";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
-import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import {
   Authenticated,
   Unauthenticated,
-  useQuery,
   useMutation,
+  useQuery,
 } from "convex/react";
-import { api } from "../convex/_generated/api";
+import { AudioWaveform, Trash2 } from "lucide-react";
 import { useState } from "react";
-import { Label } from "@/components/ui/label";
-import { Trash2 } from "lucide-react";
 import { toast } from "sonner";
-import { Greeting } from "@/components/Greeting";
+import { api } from "../convex/_generated/api";
 
 export default function Home() {
   return (
     <>
       <Authenticated>
-        {/*<UserButton />*/}
         <Content />
       </Authenticated>
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
     </>
+  );
+}
+
+function TaskForm({
+  taskTitle,
+  setTaskTitle,
+  taskDescription,
+  setTaskDescription,
+  onSubmit,
+}: {
+  taskTitle: string;
+  setTaskTitle: (value: string) => void;
+  taskDescription: string;
+  setTaskDescription: (value: string) => void;
+  onSubmit: (e: React.FormEvent) => void;
+}) {
+  return (
+    <form onSubmit={onSubmit} className="space-y-4">
+      <Label htmlFor="task-title" className="sr-only">
+        Task Title
+      </Label>
+      <Input
+        value={taskTitle}
+        onChange={(e) => setTaskTitle(e.target.value)}
+        required
+        placeholder="add a task"
+        id="task-title"
+      />
+      {/*<Label htmlFor="task-description" className="sr-only">
+        Task Description
+      </Label>
+      <Input
+        value={taskDescription}
+        onChange={(e) => setTaskDescription(e.target.value)}
+        placeholder="add more details (optional)"
+        id="task-description"
+      />*/}
+      <Button type="submit">Add Task</Button>
+    </form>
   );
 }
 
@@ -41,7 +79,7 @@ function Content() {
   const toggleTaskCompleted = useMutation(api.tasks.toggleTaskCompleted);
   const { user } = useUser();
 
-  const handleSubmit = async (e: any) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!user) return;
     await createTask({
@@ -58,102 +96,89 @@ function Content() {
     return <p>Loading...</p>;
   }
 
-  if (tasks.length === 0) {
-    return (
-      <>
-      <Greeting />
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <Label htmlFor="task-title" className="sr-only">
-            Task Title
-          </Label>
-          <Input
-            value={taskTitle}
-            onChange={(e) => setTaskTitle(e.target.value)}
-            required
-            placeholder="add a task"
-            id="task-title"
-          />
-          {/*<Label htmlFor="task-description" className="sr-only">
-            Task Description
-          </Label>
-          <Input
-            value={taskDescription}
-            onChange={(e) => setTaskDescription(e.target.value)}
-            placeholder="add more details (optional)"
-            id="task-description"
-          />*/}
-          <Button type="submit">Add Task</Button>
-        </form>
-        <p className="my-12">No tasks found.</p>
-      </>
-    );
-  }
-
   return (
     <>
       <Greeting />
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <Label htmlFor="task-title" className="sr-only">
-          Task Title
-        </Label>
-        <Input
-          value={taskTitle}
-          onChange={(e) => setTaskTitle(e.target.value)}
-          required
-          placeholder="add a task"
-          id="task-title"
-        />
-        {/*<Label htmlFor="task-description" className="sr-only">
-          Task Description
-        </Label>
-        <Input
-          value={taskDescription}
-          onChange={(e) => setTaskDescription(e.target.value)}
-          placeholder="add more details (optional)"
-          id="task-description"
-        />*/}
-        <Button type="submit">Add Task</Button>
-      </form>
-      <div className="my-12 grid gap-y-2">
-        {tasks.map((task) => (
-          <div key={task._id} className="flex items-center gap-2">
-            <Checkbox
-              checked={task.completed}
-              onCheckedChange={(checked) => {
-                if (typeof checked === "boolean") {
+      <TaskForm
+        taskTitle={taskTitle}
+        setTaskTitle={setTaskTitle}
+        taskDescription={taskDescription}
+        setTaskDescription={setTaskDescription}
+        onSubmit={handleSubmit}
+      />
+
+      {tasks.length === 0 ? (
+        <>
+          <div className="flex items-center justify-center">
+            <div className="flex flex-col">
+              <AudioWaveform className="text-muted mx-auto size-32" />
+              <p className="text-muted-foreground my-12 text-lg font-semibold">
+                Woohoo! You've completed all your tasks
+              </p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="my-12 grid gap-y-2">
+          {tasks.map((task) => (
+            <div key={task._id} className="flex items-center">
+              <Button
+                variant="ghost"
+                className="flex h-auto items-center justify-start gap-2 p-2"
+                onClick={() => {
+                  const newCompleted = !task.completed;
                   toggleTaskCompleted({
                     taskId: task._id,
-                    completed: checked,
+                    completed: newCompleted,
                   });
-                  if (checked) {
+                  if (newCompleted) {
                     toast.success("Task complete");
                   } else {
                     toast.warning("Task incomplete");
                   }
-                }
-              }}
-            />
-            <p
-              className={cn(
-                task.completed ? "text-muted-foreground line-through" : null,
-              )}
-            >
-              {task.taskTitle}
-            </p>
-            <Button
-              variant={"destructive"}
-              onClick={() => {
-                deleteTask({ taskId: task._id });
-                toast.error("Task deleted");
-              }}
-              className="ml-auto"
-              size={"icon"}
-            >
-              <Trash2 className="size-4" />
-            </Button>
-          </div>
-        ))}
-      </div>
+                }}
+              >
+                <Checkbox
+                  checked={task.completed}
+                  onCheckedChange={(checked) => {
+                    if (typeof checked === "boolean") {
+                      toggleTaskCompleted({
+                        taskId: task._id,
+                        completed: checked,
+                      });
+                      if (checked) {
+                        toast.success("Task complete");
+                      } else {
+                        toast.warning("Task incomplete");
+                      }
+                    }
+                  }}
+                />
+                <p
+                  className={cn(
+                    task.completed
+                      ? "text-muted-foreground line-through"
+                      : null,
+                  )}
+                >
+                  {task.taskTitle}
+                </p>
+              </Button>
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  deleteTask({ taskId: task._id });
+                  toast.error("Task deleted");
+                }}
+                className="group ml-auto"
+                size={"icon"}
+              >
+                <Trash2 className="group-hover:text-destructive size-4" />
+              </Button>
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,40 @@
+import { AudioWaveform } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 import { Link } from "./ui/link";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between pt-4 pb-8">
       <div>
-        <Link href="/">RF Next Starter</Link>
+        <Button asChild variant={"ghost"}>
+          <Link
+            href="/"
+            className="font-cal group flex items-center gap-2 text-2xl !no-underline"
+          >
+            <AudioWaveform className="size-6 transition-colors group-hover:text-orange-500" />
+            Cadence
+          </Link>
+        </Button>
       </div>
-      <div className="flex items-center gap-4">
-        <Link href="/pricing">Pricing</Link>
-        <Link href="#">About</Link>
-        <Link href="#">Contact</Link>
+      <div className="flex items-center gap-1">
+        <Button asChild variant={"ghost"}>
+          <Link href="/pricing" className="!no-underline">
+            Pricing
+          </Link>
+        </Button>
+
+        <Button asChild variant={"ghost"}>
+          <Link href="#" className="!no-underline">
+            About
+          </Link>
+        </Button>
+
+        <Button asChild variant={"ghost"}>
+          <Link href="#" className="!no-underline">
+            Contact
+          </Link>
+        </Button>
         <ModeToggle />
       </div>
     </nav>
